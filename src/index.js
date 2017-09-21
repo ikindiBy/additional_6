07f2c308ff3ebe7d05590278ff984aff79bca100
ikindiBy <ikindi@mail.ru>
2017-09-21 15:26:43 +0300

    additional_6/fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function zeros(expression) {
  	let arr = expression.split('*');
-	console.log(arr);
 	let result = 0;
 	let fvs = 0;
 	let tws = 0;
@@ -8,24 +7,15 @@ module.exports = function zeros(expression) {
 		if (arr[i].indexOf('!!') > 0) {
 			let sourceNumber = +arr[i].substring(0, arr[i].indexOf('!!'));
 			if (sourceNumber%2 == 0) {
-			 // result += tens(sourceNumber);
 			 result = result + tens(sourceNumber)+fivesForDF(sourceNumber);
 			 tws += twoes(sourceNumber);
-			 // fvs += fivesForDF(sourceNumber);
-			 console.log('in  !! - ',sourceNumber, 'twoes-', twoes(sourceNumber),
-				'fvs-' , fives(sourceNumber),
-				'tens-', fivesForDF(sourceNumber));
 			} else {
 				fvs += fives(sourceNumber);
-				console.log(sourceNumber,  'fvs-' , fives(sourceNumber));
 			}
 		} else {
 			let sourceNumber = +arr[i].substring(0, arr[i].indexOf('!'));
 			result += tens(sourceNumber)+fives(sourceNumber);
 			tws += twoes(sourceNumber);
-			console.log('in  ! - ',sourceNumber, 'twoes-', twoes(sourceNumber),
-				'fvs-' , fives(sourceNumber),
-				'tens-', tens(sourceNumber));
 		}
 	};
 	return (fvs >= tws) ? result+tws : result+fvs;
@@ -36,7 +26,6 @@ module.exports = function zeros(expression) {
 		let count = 0;
 		for (let i=1; i<= n; i++) {
 			if (i%5 == 0 && i%2 !== 0)  {
-				console.log(i);
 				let num = i;
 				while(num%5 == 0 && num>1) {
 					count++;
@@ -44,7 +33,6 @@ module.exports = function zeros(expression) {
 				}
 			}
 			if (i%50 == 0 && i/10 >=5) {
-				console.log(i);
 				let num = i;
 				while(num%5 == 0 && num>1) {
 					count++;
@@ -82,7 +70,6 @@ module.exports = function zeros(expression) {
 		let count = 0;
 		for (let i=2; i<= n; i=i+2) {
 			if (i%5 == 0 && i%2 !== 0)  {
-				console.log(i);
 				let num = i;
 				while(num%5 == 0 && num>1) {
 					count++;
@@ -90,7 +77,6 @@ module.exports = function zeros(expression) {
 				}
 			}
 			if (i%50 == 0 && i/10 >=5) {
-				console.log(i);
 				let num = i;
 				while(num%5 == 0 && num>1) {
 					count++;
