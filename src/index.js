3ac6a0f3a27f3c7782f5773aa278b6e949fbd1f7
ikindiBy <ikindi@mail.ru>
2017-09-21 14:20:14 +0300

    additional_6/fix

--- src/index.js
+++ src/index.js
@@ -1,41 +1,60 @@
 module.exports = function zeros(expression) {
  	let arr = expression.split('*');
+	console.log(arr);
 	let result = 0;
 	let fvs = 0;
 	let tws = 0;
-
 	for (let i=0; i<arr.length; i++) {
 		if (arr[i].indexOf('!!') > 0) {
 			let sourceNumber = +arr[i].substring(0, arr[i].indexOf('!!'));
 			if (sourceNumber%2 == 0) {
-			 result += tens(sourceNumber);
+			 // result += tens(sourceNumber);
+			 result = result + tens(sourceNumber)+fivesForDF(sourceNumber);
 			 tws += twoes(sourceNumber);
+			 // fvs += fivesForDF(sourceNumber);
+			 console.log('in  !! - ',sourceNumber, 'twoes-', twoes(sourceNumber),
+				'fvs-' , fives(sourceNumber),
+				'tens-', fivesForDF(sourceNumber));
 			} else {
 				fvs += fives(sourceNumber);
+				console.log(sourceNumber,  'fvs-' , fives(sourceNumber));
 			}
 		} else {
 			let sourceNumber = +arr[i].substring(0, arr[i].indexOf('!'));
-		result += tens(sourceNumber)+fives(sourceNumber);
-		tws += twoes(sourceNumber);
+			result += tens(sourceNumber)+fives(sourceNumber);
+			tws += twoes(sourceNumber);
+			console.log('in  ! - ',sourceNumber, 'twoes-', twoes(sourceNumber),
+				'fvs-' , fives(sourceNumber),
+				'tens-', tens(sourceNumber));
 		}
 	};
 	return (fvs >= tws) ? result+tws : result+fvs;
+
 };
 
-function fives(n) {
+	function fives(n) {
 		let count = 0;
 		for (let i=1; i<= n; i++) {
-			if (i%5 == 0 && i%2 !== 0) {
+			if (i%5 == 0 && i%2 !== 0)  {
+				console.log(i);
 				let num = i;
 				while(num%5 == 0 && num>1) {
 					count++;
 					num = num/5;
 				}
 			}
-				
+			if (i%50 == 0 && i/10 >=5) {
+				console.log(i);
+				let num = i;
+				while(num%5 == 0 && num>1) {
+					count++;
+					num = num/50;
+				}
+			}	
 		}
 		return count;
 	};
+
 
 	function twoes(n) {
 		let count = 0;
@@ -55,6 +74,29 @@ function fives(n) {
 					num = num/10;
 				}
 			}
+		}
+		return count;
+	};
+
+	function fivesForDF(n) {
+		let count = 0;
+		for (let i=2; i<= n; i=i+2) {
+			if (i%5 == 0 && i%2 !== 0)  {
+				console.log(i);
+				let num = i;
+				while(num%5 == 0 && num>1) {
+					count++;
+					num = num/5;
+				}
+			}
+			if (i%50 == 0 && i/10 >=5) {
+				console.log(i);
+				let num = i;
+				while(num%5 == 0 && num>1) {
+					count++;
+					num = num/50;
+				}
+			}	
 		}
 		return count;
 	};
